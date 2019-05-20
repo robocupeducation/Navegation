ba22ab8496ce69d04f7db9b49bc673ddb3d0c0c1
María Belén <mariabelentualombo96@hotmail.com>
2019-05-20 18:15:41 +0200

    Update mover.cpp

--- src/mover.cpp
+++ src/mover.cpp
@@ -1,24 +1,49 @@
 
+#include "ros/ros.h"
 #include "actionlib/client/simple_action_client.h"
 #include "move_base_msgs/MoveBaseAction.h"
 #include "geometry_msgs/PoseStamped.h"
 #include <string>
 #include "geometry_msgs/Twist.h"
 
+const float Pi = 3.141595;
+
+class Turner
+{
+private:
+  ros::NodeHandle n;
+  ros::Publisher num_pub;
+
+public:
+
+  Turner()
+  {
+    num_pub = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
+  }
+
+  void step()
+  {
+    geometry_msgs::Twist vel;
+    vel.angular.z = Pi / 4.0;
+    num_pub.publish(vel);
+  }
+
+};
+
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "mover_publisher");
-  ros::NodeHandle n("~");
-  ros::Publisher num_pub = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
+  Turner turner;
+
   ros::Rate loop_rate(10);
+
   while(ros::ok()){
-    
-    geometry_msgs::Twist vel;
-    vel.angular.z = 0.2;
-    num_pub.publish(vel);
-    loop_rate.sleep();
+
+    turner.step();
     ros::spinOnce();
+    loop_rate.sleep();
+
   }
   return 0;
 }
